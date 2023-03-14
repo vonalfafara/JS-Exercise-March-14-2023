6f5b280b5b4c1d7fdc19635ca881265f6374963d
tritonmax26 <stanleybalmores2013@gmail.com>
2023-03-14 15:40:05 +0800

    tried

---
+++ stan-extention.js
@@ -0,0 +1,6 @@
+// 2. Write a Javascript program to get the extension of a filename.
+
+filename = "system.html"
+console.log(filename.split('.').pop());
+filename = "abc.js"
+console.log(filename.split('.').pop());
