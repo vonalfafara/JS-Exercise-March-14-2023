c17cf7debce6915450cf58b45090fcf8264d2014
OwenXV <owensanbuenaventura@hotmail.com>
2023-03-14 15:06:04 +0800

    update

--- problem2.js
+++ problem2.js
@@ -1,5 +1,5 @@
 let fileName ="problem2.js"
-let splitText= fileName.split(".")
-let  extentsion=splitText.pop()
+let splitText= fileName.split(".").pop()
+// let extentsion=splitText.pop()
 
 console.log(splitText)
