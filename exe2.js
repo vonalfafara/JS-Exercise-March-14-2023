99d666ba2d9df38cf766aa6f802f19582dcd903c
Linggit20 <botonesallan7@gmai.com>
2023-03-14 15:01:55 +0800

    rewrite the solution

--- exe2.js
+++ exe2.js
@@ -1,14 +1,16 @@
 // 2. Write a Javascript program to get the extension of a filename.
 
-const fileName = ["index.html"]
+// const fileName = ["index.html"]
 
-const extension = fileName.map(name => {
-  const newValue = name.split(".")
-//   console.log(newValue)
+// const extension = fileName.map(name => {
+//   const newValue = name.split(".")
+// //   console.log(newValue)
 
-  return newValue.slice(1).pop()
+//   return newValue.slice(1).pop()
 
+// });
+const fileName = "index.html";
 
-});
+const extension = fileName.split(".").pop();
 
 console.log(extension)
