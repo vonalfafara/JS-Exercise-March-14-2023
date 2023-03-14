e418e2de2f0a5447f8f1c256e60a70de5b092204
OwenXV <owensanbuenaventura@hotmail.com>
2023-03-14 14:49:26 +0800

    solution

---
+++ problem1.js
@@ -0,0 +1,5 @@
+function myFunction(x,y){
+    return (x==100 || y==100) || (x+y==100);
+}
+
+console.log(myFunction(99+1))
---
+++ problem2.js
@@ -0,0 +1,4 @@
+let fileName ="problem2.js"
+let extension = fileName.split(".").pop();
+
+console.log(extension)
