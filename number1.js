50b9fdcbd94467ac171dc91cbc748bc9f776563d
unknown <dev.andrewearl@gmail.com>
2023-03-14 15:10:52 +0800

    Only Exercise 1

---
+++ number1.js
@@ -0,0 +1,15 @@
+function checker(x,y) {
+
+  if (x == 100) {
+    console.log(x == 100)
+  } else if (y == 100) {
+    console.log(y == 100)
+  } else if (x + y == 100) {
+    console.log(x + y == 100)
+  } else {
+    console.log("X and Y is not 100, X + Y is not equal to 100")
+  }
+
+}
+
+checker(50,50)
