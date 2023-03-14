4913033aa3ebb49c83b710b4f2865bb22b10c552
tritonmax26 <stanleybalmores2013@gmail.com>
2023-03-14 15:06:18 +0800

    tried but failed

--- stan-numbers.js
+++ stan-numbers.js
@@ -1,17 +1,17 @@
 // 1. Write a Javascript program to check two numbers and return true if one of the numbers is 100 or the sum of both numbers is 100.
 
-let a = 100
-let b = 100
-let x = 3
-let y = x+3
+let x = 100
+let y = 30
+let z = x+y
 
 // console.log (y);
 
-if (y>100) {
+if (z<100) {
     console.log ("true");
 } else {
     console.log ("false");
 }
+
 
 
 
