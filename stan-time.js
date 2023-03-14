e22f07ff26e9e5268a2ac8d760b5e5566b79de96
tritonmax26 <stanleybalmores2013@gmail.com>
2023-03-14 15:30:47 +0800

    tried but failed

--- stan-time.js
+++ stan-time.js
@@ -1,5 +1,10 @@
-const date = new Date().getMonth
-let time = date
+// 3. Write a Javascript program to get the current date in a specific format (mm-dd-yyyy, mm/dd/yyyy)
 
-console.log (date)
+let date = new Date();
+let month = date.getMonth () + 1
+let day = date.getDay ()
+let year = date.getFullYear()
+
+
+console.log (month + "/" + day + "/" + year)
 
