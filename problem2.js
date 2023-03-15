b9b07d95af2b113f09c6b0dbbb29b33c1f7e0c98
CalixCarlo <carloalbis59@gmail.com>
2023-03-15 00:23:21 -0700

    activity

---
+++ problem2.js
@@ -0,0 +1,37 @@
+const items = [
+  {
+    item: "Book",
+    price: 50,
+    forSale: true
+  },
+  {
+    item: "Pen",
+    price: 10,
+    forSale: true
+  },
+  {
+    item: "Paper",
+    price: 25,
+    forSale: false
+  },
+  {
+    item: "Glue",
+    price: 5,
+    forSale: true
+  },
+  {
+    item: "Table",
+    price: 100,
+    forSale: false
+  },
+];
+
+function displayForSale(array) {
+  array.forEach(item => {
+    if (item.forSale) {
+      console.log(`${item.item} - $${item.price}`);
+    }
+  });
+}
+
+displayForSale(items);
