45ce992261d26def49a961554dd50c47011ea21a
CalixCarlo <carloalbis59@gmail.com>
2023-03-15 00:04:17 -0700

    0315 activity

--- extension.js
+++
@@ -1,4 +0,0 @@
-let filename = "index.js";
-let extension = filename.split(".").pop();
-
-console.log(extension); 
---
+++ problem1.js
@@ -0,0 +1,5 @@
+const myArray = [1, 2, 3, 4, 5];
+const n = 3;
+
+const firstNItems = getFirstNItems(myArray, n);
+console.log(firstNItems); // prints [1, 2, 3]
