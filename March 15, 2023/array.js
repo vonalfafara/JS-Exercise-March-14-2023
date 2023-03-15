3735f3da8572bca942550911ca85df15bf6d5e49
buniagj <126451267+buniagj@users.noreply.github.com>
2023-03-15 14:44:24 +0800

    Update array

--- March 15, 2023/array.js
+++ March 15, 2023/array.js
@@ -1,9 +1,9 @@
-
 const array = ["fuchsia", "rouge", "taffy", "blush", "cerise"];
 const n = 3;
+const result = getElements(array, n);
 
 function getElements(array, n) {
   return array.slice(0, n);
 }
-const result = getElements(array, n);
+
 console.log(result);
