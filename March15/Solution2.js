789bb9d103a645a7a84191591cc5066f7155dfb2
unknown <dev.andrewearl@gmail.com>
2023-03-15 14:39:50 +0800

    Solutions for March 15 Exercises

---
+++ March15/Solution2.js
@@ -0,0 +1,37 @@
+const items = [
+  {
+    item: "Book",
+    price: 50,
+    forSale: true
+  },
+  {
+    item: "Pen",
+    price: 10,
+    forSale: true
+  },
+  {
+    item: "Paper",
+    price: 25,
+    forSale: false
+  },
+  {
+    item: "Glue",
+    price: 5,
+    forSale: true
+  },
+  {
+    item: "Table",
+    price: 100,
+    forSale: false
+  },
+]
+
+function displayForSale(array) {
+  let n = array.length
+
+  for (let i = 0; i < n; i++) {
+      console.log(array[i].item,array[i].price,array[i].forSale)
+   }
+}
+
+displayForSale(items)
