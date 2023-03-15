92d57d9a5c18782d986696570b82c565717bbefe
tritonmax26 <stanleybalmores2013@gmail.com>
2023-03-15 15:03:57 +0800

    updated stan

--- March 15/stan.js
+++ March 15/stan.js
@@ -1,11 +1,14 @@
 // [Array]
 // 1. Write a Javascript function that returns the first N items of an array, where N is the number of items. The function must accept 2 parameters: the array and the number of items to be returned.
 
-const array = [1,2,3,4]
-const n = 5
+const array = [1,2,3,4,5,6,7,8,9]
+const n = 4
 
-function getElements(array, numberOfItems) {
-  ...
+
+
+function getElements(array, n) {
+    array.splice (0,n,array)
+    return getElements
 }
 
 const result = getElements(array, n)
