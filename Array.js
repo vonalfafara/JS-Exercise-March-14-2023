8457c9af99fce9927b724d096cd3a584af47cc73
kd-engineer <durantekevin004@gmail.com>
2023-03-15 14:59:01 +0800

    Solutions for the problem

---
+++ Array.js
@@ -0,0 +1,13 @@
+/*[Array]
+1. Write a Javascript function that returns the first N items of an array, where N is the number of items. The function must accept 2 parameters: the array and the number of items to be returned.*/
+
+const array = [125, 5454, 54, 4, 64, 2656, 54, 545]
+const n = array.length
+
+function getElements(array, numberOfItems) {
+    i = console.log(array[0])
+    return(i, n)
+}
+
+const result = getElements(array, n)
+console.log(result)
