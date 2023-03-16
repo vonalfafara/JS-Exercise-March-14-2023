fa77cc4b49b852a2ea7186dbd1003916a2d7660e
myke023 <pmiranda3333@gmail.com>
2023-03-16 14:54:08 +0800

    make changes

--- march152023/solution2.js
+++ march152023/solution2.js
@@ -1,45 +1,42 @@
-// Write a Javascript function that displays items that are for sale from an array of objects
-
-function displayItemsForSale(items) {
-    const forSaleItems = items.filter(item => item.forSale === true);
-    for (let i = 0; i < forSaleItems.length; i++) {
-        console.log(forSaleItems[i].item + " is for sale at $" + forSaleItems[i].price);
-    }
-}
-
-
-// Here's how to use the function:
+// [Array of Objects]
+// 2. Write a Javascript function that displays items that are for sale from an array of objects.
 
 const items = [
     {
-        item: "Book",
-        price: 50,
-        forSale: true
+      item: "Book",
+      price: 50,
+      forSale: true,
     },
     {
-        item: "Pen",
-        price: 10,
-        forSale: true
+      item: "Pen",
+      price: 10,
+      forSale: true,
     },
     {
-        item: "Paper",
-        price: 25,
-        forSale: false
+      item: "Paper",
+      price: 25,
+      forSale: false,
     },
     {
-        item: "Glue",
-        price: 5,
-        forSale: true
+      item: "Glue",
+      price: 5,
+      forSale: true,
     },
     {
-        item: "Table",
-        price: 100,
-        forSale: false
+      item: "Table",
+      price: 100,
+      forSale: false,
     },
-];
-
-displayItemsForSale(items);
-
-// Book is for sale at $50
-// Pen is for sale at $10
-// Glue is for sale at $5
+  ]
+  
+  function displayForSale(array) {
+    for (let index = 0; index < array.length; index++) {
+      if (array[index].forSale == true) {
+        console.log(
+          `${array[index].item} is for sale with a price of ${array[index].price}`
+        )
+      }
+    }
+  }
+  
+  displayForSale(items)
