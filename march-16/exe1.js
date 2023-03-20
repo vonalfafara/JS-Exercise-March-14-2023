436458a842ecc509eaf8da46d63a2ab1e4143a51
Linggit20 <botonesallan7@gmai.com>
2023-03-20 13:13:34 +0800

    arrange files

--- march-16/exe1.js
+++ march-16/exe1.js
@@ -1,37 +1,3 @@
-//  Write a function that returns the sum of even positive numbers from an array.
-// const numbers = [5, 7, 3, 4, 10, 6];
-
-// function sumOfPositveNum(updateNum) {
-//   let total = 0;
-
-//   for (let i = 0; i < updateNum.length; i++) {
-//      num = updateNum[i];
-//     if (num > 0 && num % 2 === 0) {
-//       total += num;
-//     }
-//   }
-//   return total;
-// }
-
-// console.log(sumOfPositveNum(numbers)); 
-
-
-
-// const sum = (updateNum) => {
-//   let total = 0;
-
-//   for (let i = 0; i < updateNum.length; i++) {
-    
-//          num = updateNum[i];
-//         if (num > 0 && num % 2 === 0) {
-//           total += num;
-//         }
-//       }
-//       return total;
-// }
-
-// console.log(sum(numbers)); 
-
 const numbers = [5, 7, 3, 4, 10, 6];
 
 const sum = (num) => {
