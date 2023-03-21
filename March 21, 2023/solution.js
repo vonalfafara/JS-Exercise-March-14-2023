5c4d1c02546b6d5409f86cfc0a05fc6227a1285f
KuyaW <91214926+KuyaW@users.noreply.github.com>
2023-03-21 09:35:45 +0800

    Another revision

--- March 21, 2023/solution.js
+++ March 21, 2023/solution.js
@@ -2,9 +2,6 @@ let menu = document.getElementById('menu');
 let nav = document.getElementById('myNav');
 let anchors= nav.getElementsByTagName('a');
 
-let line1 = menu.children[0];
-let line2 = menu.children[1];
-let line3 = menu.children[2];
 
 for(let anchor of anchors){
     anchor.onclick = function(){
