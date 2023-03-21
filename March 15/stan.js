4c1f30356fca98a358c64a4227b180bedb25e352
tritonmax26 <stanleybalmores2013@gmail.com>
2023-03-21 09:20:55 +0800

    coppied problem

--- March 15/stan.js
+++ March 15/stan.js
@@ -2,7 +2,7 @@
 // 1. Write a Javascript function that returns the first N items of an array, where N is the number of items. The function must accept 2 parameters: the array and the number of items to be returned.
 
 const array = [1,2,3,4,5,6,7,8,9]
-const n = 6
+const n = 11
 
 
 array.splice (n,array.length)
